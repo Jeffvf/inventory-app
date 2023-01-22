0b9e2377d495d7e7907ae1b0b53b1a05508f5c8d
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-22 00:22:18 -0300

    refactor: nome dos campos da colecao

--- models/supplier.js
+++ models/supplier.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema();
 
 const SupplierSchema = new Schema({
-  nome: { type: String, required: true, maxLength: 60 },
+  name: { type: String, required: true, maxLength: 60 },
   cnpj: { type: Number, required: true },
-  cidade: { type: String },
-  estado: { type: String, maxLength: 2, required: true }
+  city: { type: String },
+  state: { type: String, maxLength: 2, required: true }
 });
 
 SupplierSchema.virtual("url").get(function () {
