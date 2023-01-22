eee8824972f211ce2b67f5f1b3165a3c96aca497
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-22 00:23:04 -0300

    fix: atribuicao de metodo Schema do mongoose

--- models/supplier.js
+++ models/supplier.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
 
 const SupplierSchema = new Schema({
   name: { type: String, required: true, maxLength: 60 },
