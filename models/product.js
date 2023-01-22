e5c367fc0ad152a44575e2b6abd07c9257dfbf77
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-22 19:28:24 -0300

    feat: preco do produto formatado

--- models/product.js
+++ models/product.js
@@ -15,4 +15,18 @@ ProductSchema.virtual("url").get(function() {
   return `/inventory/product/${this._id}`;
 });
 
+ProductSchema.virtual('formattedPrice').get(function(){
+  const price = this.price.toString();
+
+  let [integerPart, decimal] = price.split('.');
+  if(decimal == undefined){
+    decimal = "00"
+  }
+  else if(decimal.length == 1){
+    decimal = decimal.concat('0');
+  }
+
+  return integerPart.concat(',',decimal);
+});
+
 module.exports = mongoose.model("Product", ProductSchema);
