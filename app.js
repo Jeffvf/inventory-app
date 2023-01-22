cff22ba27ab186634c348416b1d52622cff4d144
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-22 15:07:22 -0300

    fix: chamada de conexao ao banco de dados

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const inventoryRouter = require('./routes/inventory');
 
 var app = express();
 
-const mongoose = requise('mongoose');
+const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-const mongoDB = 'mongodb+srv://admin:157359@cluster0.ognltns.mongodb.net/inventory?retryWrites=true&w=majority';
-
-main.catch(err => console.log(err));
-async function main(){
-  await mongoose.connect(mongoDB);
-}
+const devURL = 'mongodb+srv://admin:157359@cluster0.ognltns.mongodb.net/inventory?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || devURL;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/inventory', inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
