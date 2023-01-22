1b5ce287d1dd401a6370e84fc44d0e6afb36fec2
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-22 20:34:11 -0300

    feat: lista fornecedores e suas informacoes

--- controllers/supplierController.js
+++ controllers/supplierController.js
@@ -1,11 +1,46 @@
 const Supplier = require('../models/supplier');
+const Product = require('../models/product');
+const async = require('async');
 
-exports.supplierList = (req, res) => {
-  res.send('NOT IMPLEMENTED: Supplier list');
+exports.supplierList = (req, res, next) => {
+  Supplier.find({}, 'name state')
+    .exec(function(err, result) {
+      if(err){
+        return next(err);
+      }
+      res.render('supplier_list', {
+        title: 'Supplier List',
+        supplier_list: result
+      });
+    });
 }
 
-exports.supplierDetail = (req, res) => {
-  res.send('NOT IMPLEMENTED: Supplier detail');
+exports.supplierDetail = (req, res, next) => {
+  async.parallel(
+    {
+      supplier(callback){
+        Supplier.findById(req.params.id).exec(callback);
+      },
+      products(callback){
+        Product.find({ supplier: req.params.id }, 'name').exec(callback);
+      }
+    },
+    (err, results) => {
+      if(err){
+        next(err);
+      }
+      if(results.supplier == null){
+        const error = new Error('Supplier not found');
+        error.status = 404;
+
+        return next(error);
+      }
+      res.render('supplier_detail', {
+        supplier: results.supplier,
+        supplier_products: results.products
+      });
+    }
+  );
 }
 
 exports.supplierCreateGet = (req, res) => {
