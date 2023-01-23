050a27ac50c2ce143ed478a0b6229011e551ea4e
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-22 22:59:07 -0300

    feat: lista categorias e seus produtos

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,6 @@
 const Category = require('../models/category');
+const Product = require('../models/product');
+const async = require('async');
 
 exports.categoryList = (req, res, next) => {
   Category.find()
@@ -8,14 +10,37 @@ exports.categoryList = (req, res, next) => {
       return next(err);
     }
     res.render('category_list', {
-      title: 'Categories',
+      title: 'Categorias',
       categories: results
     });
   });
 }
 
-exports.categoryDetail = (req, res) => {
-  res.send('NOT IMPLEMENTED: Category detail');
+exports.categoryDetail = (req, res, next) => {
+  async.parallel(
+    {
+      category(callback){
+        Category.findById(req.params.id).exec(callback);
+      },
+      products(callback){
+        Product.find({ category: req.params.id }, 'name').exec(callback);
+      }
+    },
+    (err, results) => {
+      if(err){
+        return next(err);
+      }
+      if(results.category == null){
+        const error = new Error('Categoria não encontrada');
+        error.status = 404;
+        return next(error);
+      }
+      res.render('category_detail', {
+        category: results.category,
+        products: results.products
+      });
+    }
+  );
 }
 
 exports.categoryCreateGet = (req, res) => {
