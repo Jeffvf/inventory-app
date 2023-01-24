69b20a37b8a6fa13f59fcecf4db5055b9ec09205
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-24 11:29:49 -0300

    feat: implementa update de produto

--- controllers/productController.js
+++ controllers/productController.js
@@ -130,13 +130,108 @@ exports.productCreatePost = [
   }
 ]
 
-exports.productUpdateGet = (req, res) => {
-  res.send('NOT IMPLEMENTED: Product update GET');
+exports.productUpdateGet = (req, res, next) => {
+  async.parallel(
+    {
+      categories(callback){
+        Category.find({}, 'name').exec(callback)
+      },
+      suppliers(callback){
+        Supplier.find({}, 'name state city').exec(callback);
+      },
+      product(callback){
+        Product.findById(req.params.id).exec(callback);
+      }
+    },
+    (err, results) => {
+      if(err){
+        return next(err);
+      }
+      if(results.product == null){
+        const error = new Error('Produto não encontrado');
+        error.status = 404;
+        return next(error);
+      }
+      res.render('product_form', {
+        title: 'Atualizar produto',
+        categories: results.categories,
+        suppliers: results.suppliers,
+        product: results.product
+      });
+    }
+  );
 }
 
-exports.productUpdatePost = (req, res) => {
-  res.send('NOT IMPLEMENTED: Product update POST');
-}
+exports.productUpdatePost = [
+  body('name', 'Nome de produto inválido')
+  .trim()
+  .isLength({ min: 3 })
+  .escape(),
+  body('description', 'Descrição inválida')
+  .optional({ checkFalsy:true })
+  .trim()
+  .isLength({min: 3})
+  .escape(),
+  body('quantity', 'Quantidade inválida')
+  .optional({ checkFalsy:true })
+  .isNumeric()
+  .escape(),
+  body('price', 'Preço inválido')
+  .isNumeric()
+  .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const product = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      quantity: req.body.quantity,
+      price: req.body.price,
+      category: req.body.category,
+      supplier: [req.body.supplier],
+      _id: req.params.id
+    });
+
+    if(req.body.supplier1)
+      product.supplier.push(req.body.supplier1);
+    if(req.body.supplier2){
+      product.supplier.push(req.body.supplier2);
+    }
+
+    async.parallel(
+      {
+        categories(callback){
+          Category.find({}, 'name').exec(callback);
+        },
+        suppliers(callback){
+          Supplier.find({}, 'name state city').exec(callback)
+        }
+      },
+      (err, results) => {
+        if(err){
+          return next(err);
+        }
+        if(!errors.isEmpty()){
+          res.render('product_form', {
+            title: 'Atualizar produto',
+            categories: results.categories,
+            suppliers: results.suppliers,
+            errors: errors.array(),
+            product
+          });
+          return;
+        }
+
+        Product.findByIdAndUpdate(req.params.id, product, {}, function(err, doc) {
+          if(err){
+            return next(err);
+          }
+          res.redirect(doc.url);
+        });
+      }
+    );
+  }
+]
 
 exports.productDeleteGet = (req, res) => {
   res.send('NOT IMPLEMENTED: Product delete GET');
