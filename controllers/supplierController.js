f1ac461fbf0c38e4283e832fb40eb2cc04be9d8e
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-24 10:56:54 -0300

    feat: implementa update de fornecedores

--- controllers/supplierController.js
+++ controllers/supplierController.js
@@ -94,13 +94,71 @@ exports.supplierCreatePost = [
   }
 ];
 
-exports.supplierUpdateGet = (req, res) => {
-  res.send('NOT IMPLEMENTED: Supplier update GET');
+exports.supplierUpdateGet = (req, res, next) => {
+  Supplier.findById(req.params.id, (err, supplier) => {
+    if(err){
+      return next(err);
+    }
+    if(supplier == null){
+      const error = new Error('Fornecedor não encontrado');
+      error.status = 404;
+
+      return next(error);
+    }
+
+    res.render('supplier_form', {
+      title: 'Atualizar fornecedor',
+      supplier
+    });
+  });
 }
 
-exports.supplierUpdatePost = (req, res) => {
-  res.send('NOT IMPLEMENTED: Supplier update POST');
-}
+exports.supplierUpdatePost = [
+  body('name', 'Campo nome não deve ser vazio.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('cnpj', 'Campo CNPJ deve conter 14 caracteres')
+    .trim()
+    .isLength({ min: 14, max: 14 })
+    .escape(),
+  body('city', 'Nome de cidade inválido')
+    .optional({ checkFalsy: true })
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body('state', 'Campo estado não deve ser vazio.')
+    .trim()
+    .isLength({ min:2 })
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const supplier = new Supplier({
+      name: req.body.name,
+      cnpj: req.body.cnpj,
+      city: req.body.city,
+      state: req.body.state,
+      _id: req.params.id
+    });
+
+    if(!errors.isEmpty()){
+      res.render('supplier_form', {
+        title: 'Atualizar fornecedor',
+        errors: errors.array(),
+        supplier
+      });
+      return;
+    }
+
+    Supplier.findByIdAndUpdate(req.params.id, supplier, {}, function(err, doc){
+      if(err){
+        return next(err);
+      }
+      res.redirect(doc.url);
+    });
+  }
+]
 
 exports.supplierDeleteGet = (req, res) => {
   res.send('NOT IMPLEMENTED: Supplier delete GET');
